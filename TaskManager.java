93b909371fe65ad26234e4a4a9099eaa6339e33f
tshiambag-design <tshiambag@gmail.com>
2026-06-03 18:43:21 -0700

    Create TaskManager.java
    Added TaskManager class

---
+++ TaskManager.java
@@ -0,0 +1,27 @@
+// Manages a list of tasks
+class TaskManager {
+    private ArrayList<Task> tasks = new ArrayList<>();
+
+    public void addTask(String name, int priority) {
+        tasks.add(new Task(name, priority));
+    }
+
+    public void listTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("No tasks available.");
+            return;
+        }
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println(i + ". " + tasks.get(i));
+        }
+    }
+
+    public void markTaskDone(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            tasks.get(index).markCompleted();
+            System.out.println("Task marked as completed.");
+        } else {
+            System.out.println("Invalid task index.");
+        }
+    }
+}
